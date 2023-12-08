26fa5460aba934cb44c0fa4af033e7eef52fd173
tkdtg <152261469+tkdtg@users.noreply.github.com>
2023-12-08 18:01:09 +0900

    Update EndToEndServer.java

--- EndToEndServer.java
+++ EndToEndServer.java
@@ -1,41 +1,162 @@
 package endtoend;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.*;
 
-public class EndToEndClient {
-    private Socket socket;
-    private BufferedReader reader;
-    private BufferedWriter writer;
-    private String username;
+public class EndToEndServer {
 
-    public EndToEndClient() {
+    private static List<Socket> clients = new ArrayList<>();
+    private static List<String> wordList = new ArrayList<>();
+    private static Stack<String> stack = new Stack<>();
+    private ServerSocket serverSocket;
+    public EndToEndServer() {
         try {
-            socket = new Socket("localhost", 12345);
-            System.out.println("서버에 연결됨");
+            readText();
+            stack.push(wordList.get(0));
 
-            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            serverSocket = new ServerSocket(12345);
+            System.out.println("서버 시작. 클라이언트 연결 대기 중...");
 
-            //유저 이름 저장
-            username = JOptionPane.showInputDialog("이름을 입력하세요!");
-            if(username == null) {
-                System.exit(0);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                // 클라이언트 처리를 위한 스레드 생성
+                clients.add(clientSocket);
+                System.out.println("클라이언트 연결됨: " + clientSocket);
+
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+
+                new Thread(clientHandler).start();
+                System.out.println("clientHandler 스레드 시작");
             }
-            //게임 GUI생성
-            GameGUI gameGUI = new GameGUI(reader, writer,username);
-            gameGUI.init();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    //사전 초기화 메서드
+    public void readText() throws IOException{
+        try(BufferedReader in = new BufferedReader(new FileReader("src/endtoend/dictionary.txt"))){
+            String str;
+            while((str=in.readLine())!=null){
+                wordList.add(str);
+            }
+            Collections.shuffle(wordList);
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void initTargetword() {
+
+    }
+
+    private static class ClientHandler implements Runnable {
+        private Socket clientSocket;
+        private BufferedReader reader;
+
+        public ClientHandler(Socket clientSocket) {
+            try {
+                reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                System.out.println("reader 선언");
+                this.clientSocket = clientSocket;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            try {
+                String message;
+                String tryword;
+                System.out.println("run함수 시작");
+                System.out.println(0);
+                while ((message = reader.readLine()) != null) {
+                    System.out.println(1);
+
+                    if (message.startsWith("Game:")) { // Game에서 온 문자인 경우
+                        System.out.println("게임에서 온 문자");
+                        tryword = message.replace("Game:", "");//message에서 Game: 를 없애줌
+                        logic(tryword);
+
+                    } else { // Chat에서 온 문자인 경우
+                        System.out.println("채팅에서 온 문자");
+                        chatbroadcast(message);
+                        }
+                }
+            } catch (IOException e) {
+                System.out.println("클라이언트 종료");
+
+                if (!clientSocket.isClosed()) {
+                    try {
+                        clientSocket.close();
+                    } catch(IOException ex) {
+                        System.err.println("소켓 닫기 실패");
+                    }
+                }
+            }
+            System.out.println("run함수 끝");
+        }
+
+        public void logic(String word) {
+            String backword = stack.peek();
+            //backword(기존단어), word(사용자가 입력한 단어)
+            //끝말잇기 로직구현
+            //만족한다면 word를 매개변수로 gamewordbroadcast함수호출
+//            System.out.println("로직 함수 시작");
+//            try {
+//                gamewordbroadcast(word);
+//            }catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            System.out.println("로직 함수 종료");
+        }
+
+        //inputField에서 보낸 문자를 다른 클라이언트들에게 전송하는 메서드
+        private void chatbroadcast(String message) throws IOException{
+            clients.removeIf(Socket::isClosed);
+            for(Socket client : clients) {
+                System.out.println(4);
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+
+                writer.write(message);
+                writer.newLine();
+                writer.flush();
+                System.out.println("write완료");
+            }
+        }
+
+        //GameField에서 보낸 문자를 다른 클라이언트들에게 전송하는 메서드
+        //이떄는 다른 클라이언트들에게 전송할 때 "Game:"을 더해서 전송
+        private void gamewordbroadcast(String message) throws IOException{
+            clients.removeIf(Socket::isClosed);
+            for(Socket client : clients) {
+                System.out.println("게임브로드캐스트 시작");
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+                writer.write("Game:"+ message);
+                writer.newLine();
+                writer.flush();
+            }
+        }
+
+        //        private void chatbroadcast(String message){
+//            clients.removeIf(Socket::isClosed); // Socket이 닫혀있다면 clients에서 삭제
+//            for (Socket client : clients) {
+//                try {
+//                    PrintWriter out = new PrintWriter(client.getOutputStream());
+//                    out.println(message);
+//                    out.flush();
+//                }
+//                catch (IOException e){
+//                }
+//            }
+//        }
+
+    }
+
     public static void main(String[] args) {
-        new EndToEndClient();
+        new EndToEndServer();
     }
 }
-
