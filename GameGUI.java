3c39d447854116773819104f8ff2468ea593eeff
Dayno98 <118802137+Dayno98@users.noreply.github.com>
2023-12-08 13:57:22 +0900

    Add files via upload

--- GameGUI.java
+++ GameGUI.java
@@ -13,8 +13,11 @@ public class GameGUI {
     private PrintWriter writer;
 
     private JFrame frame;
-    private JTextField inputField;
-    private JTextArea chatArea;
+    private JTextField inputField,gameinputField;
+    private JTextArea chatArea,preanswer;
+    private JPanel chatP,gameP;
+    private JScrollPane scroll,scroll2;
+    private JLabel timecount;
 
     public GameGUI(BufferedReader reader, PrintWriter writer) {
         this.reader = reader;
@@ -24,11 +27,37 @@ public class GameGUI {
     public void init() {
         frame = new JFrame("끝말잇기 게임");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);
+        frame.setSize(400, 600);
+        frame.setResizable(false);
+        frame.setLayout(new GridLayout(2,1));
 
         inputField = new JTextField();
+        gameinputField = new JTextField();
+
+        inputField.addActionListener(e->{//enter 쳐도 입력가능하게
+            String word = inputField.getText();
+            sendMessage(word);
+                });
+
+        gameinputField.addActionListener(e->{//enter 쳐도 입력가능하게
+            String gameword = gameinputField.getText();
+            System.out.println(gameword); //여기서 텍스트가 조건을 통과하면 입력되도록
+            preanswer.append(", "+gameword);
+            gameinputField.setText("");
+        });
+
         chatArea = new JTextArea();
         chatArea.setEditable(false);
+        scroll = new JScrollPane(chatArea);
+
+        preanswer = new JTextArea(2,1);
+        preanswer.setFocusable(false);
+        preanswer.setSize(400,100);
+        scroll2 = new JScrollPane(preanswer);
+
+
+
+
 
         JButton submitButton = new JButton("제출");
         submitButton.addActionListener(new ActionListener() {
@@ -39,14 +68,66 @@ public class GameGUI {
             }
         });
 
+        JButton gamesubmitButton = new JButton("제출");
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String gameword = gameinputField.getText();
+                System.out.println(gameword);//여기서 텍스트가 조건을 통과하면 입력되도록
+                preanswer.append(", "+gameword);
+                gameinputField.setText("");
+            }
+        });
+
+        gameP = new JPanel();
+        gameP.setSize(400,300);
+        gameP.setLayout(new BorderLayout());
+        gameP.setBackground(Color.WHITE);
+
+        JButton targetword = new JButton("targetword");
+        targetword.setFocusable(false);
+
+        targetword.setSize(150,100);
+        gameP.add(targetword,BorderLayout.CENTER);
+        gameP.add(scroll2,BorderLayout.NORTH);
+
+        //timecount = new JLabel("timer");
+        //timecount.setPreferredSize(new Dimension(50,50));
+        //gameP.add(timecount,null);
+        // timecount.setLocation(200,200);
+        frame.add(gameP);
+
+        JPanel gameChatP = new JPanel();
+        gameChatP.setLayout(new BorderLayout());
+        gameChatP.add(gameinputField, BorderLayout.CENTER);
+        gameChatP.add(gamesubmitButton, BorderLayout.EAST);
+
+
+        gameP.add(gameChatP, BorderLayout.SOUTH);
+
+
+
+
+
+        //chatP 영역 아래 채팅 입력 부분.
         JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout());
         panel.add(inputField, BorderLayout.CENTER);
         panel.add(submitButton, BorderLayout.EAST);
 
+        chatP = new JPanel();
+        chatP.setSize(400,300);
+
+        chatP.setLayout(new BorderLayout());
+        chatP.add(scroll, BorderLayout.CENTER); //스크롤 기능이 있는 textfiled 추가
+        chatP.add(panel, BorderLayout.SOUTH);
+        frame.add(chatP);
+
+        /*
         frame.getContentPane().setLayout(new BorderLayout());
         frame.getContentPane().add(chatArea, BorderLayout.CENTER);
         frame.getContentPane().add(panel, BorderLayout.SOUTH);
+         */
 
         frame.setVisible(true);
 
