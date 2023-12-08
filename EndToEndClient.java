51f30bfc0e7fe6e0fe8ab8ce6b5db88afe60e8d0
tkdtg <152261469+tkdtg@users.noreply.github.com>
2023-12-08 18:02:03 +0900

    Update EndToEndClient.java

--- EndToEndClient.java
+++ EndToEndClient.java
@@ -4,16 +4,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
 public class EndToEndClient {
     private Socket socket;
     private BufferedReader reader;
-    private PrintWriter writer;
+    private BufferedWriter writer;
     private String username;
 
     public EndToEndClient() {
@@ -22,12 +19,14 @@ public class EndToEndClient {
             System.out.println("서버에 연결됨");
 
             reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            writer = new PrintWriter(socket.getOutputStream(), true);
+            writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
+            //유저 이름 저장
             username = JOptionPane.showInputDialog("이름을 입력하세요!");
             if(username == null) {
                 System.exit(0);
             }
+            //게임 GUI생성
             GameGUI gameGUI = new GameGUI(reader, writer,username);
             gameGUI.init();
         } catch (IOException e) {
